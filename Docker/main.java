70f09de35ba7dcad23bda7905c5de5c7d38edab7
Jyoti Panigrahi <50869609+pjyoti1395@users.noreply.github.com>
2025-08-05 14:48:51 +0530

    Create main.java

---
+++ Docker/main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Date currentDate = new Date();
+        System.out.println("Hello Docker "  + currentDate);
+        System.out.println("This is  my first java application");
+    }
+}
